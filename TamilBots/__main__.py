fc3a3a59cff8e558ccb33f1fdafbb6a7d6c3cc5c
sarangsahajan <88932889+sarangsahajan@users.noreply.github.com>
2021-12-30 14:51:04 +0530

    Add files via upload

--- TamilBots/__main__.py
+++ TamilBots/__main__.py
@@ -1,232 +1,63 @@
-import os
-import asyncio
-from pyrogram import Client, filters, __version__
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram.errors import FloodWait
-
 from config import OWNER_ID
 from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
 from TamilBots.modules import *
 from pyrogram import idle, filters
-from config import FORCE_SUB_CHANNEL, ADMINS
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup
 from pyrogram.types import InlineKeyboardButton
-from TamilBots import app as Bot 
-from TamilBots import  LOGGER
+from TamilBots import app, LOGGER
 from TamilBots.TamilBots import ignore_blacklisted_users
 from TamilBots.sql.chat_sql import add_chat_to_db
-from bot import Bot
-from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID
-from helper_func import subscribed, get_messages
 
-START_TEXT = """ Hai {}, 
-Iam a song download Bot 🙂
-"""
+start_text = """
+👋 𝗛𝗲𝗹𝗹𝗼 [{}](tg://user?id={}),
 
-CMDS_TEXT = """
-Hey {} This are this bots power🙂
-"""
+\n\n𝗜 𝗔𝗺 🎸𝐒𝐨𝐧𝐠 𝐏𝐥𝐚𝐲 𝐁𝐨𝐭[🎶](https://telegra.ph/file/6cb884fe1cb943ec12df1.mp4)
 
-ABOUT_TEXT = """
-Aʙᴏᴜᴛ Mᴇ
-╭──[ 🔅 Rᴇᴇʟᴏᴀᴅ Mᴇᴅɪᴀ 🔅 ]───⍟
-│
-├🔹Dᴇᴠᴇʟᴏᴘᴇʀ : [Nᴏʙᴏᴅʏ](https://t.me/n_ob_od_y)
-│
-├🔹Cʜᴀɴɴᴇʟ : [Rᴇᴇʟᴏᴀᴅ Mᴇᴅɪᴀ](https://t.me/ReeloadMedia)
-│
-├🔸Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : [RMCʜᴀᴛs](https://t.me/ReeloadMediaChats)
-│
-╰─────────[ 😎 ]────────⍟
-"""
-MUSIC = """ **🎧MUSIC**
-You can also use this feature in group too
-➩ /music <songname> or /song <songname> artist(optional)>: uploads the song in it's best quality available
-You can also use these commands
-"""
- 
-  
-LYRICS = """ **🎶LYRICS🎶**
-You can also use this feature in group too
-➩ /lyrics <songname>: uploads the lyrics of song
+I'M Music Bot By @TamilBots 🤖
+
+𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗡𝗮𝗺𝗲 𝗢𝗳 𝗧𝗵𝗲 𝗦𝗼𝗻𝗴 𝗬𝗼𝘂 𝗪𝗮𝗻𝘁... 😍🥰🤗
+
+𝐄𝐠. ```/song Faded```
 """
 
-YOUTUBE = """  **📽️YOUTUBE📽️**
-you can also use inline for search YouTube video or song
+owner_help = """
+/blacklist user_id
+/unblacklist user_id
+/broadcast message to send
+/eval python code
+/chatlist get list of all chats
 """
 
 
-START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('Support📕', url=f"https://telegram.me/ReeloadMedia"), 
-        InlineKeyboardButton(text="SEARCH🔎", switch_inline_query_current_chat="")
-        ],[
-        InlineKeyboardButton(text="➕⚡Add ME TO YOUR GROUP⚡➕", url="t.me/RMSongsbot?startgroup=true"), 
-        ],[     
-        InlineKeyboardButton('HELPℹ️', callback_data ='cmds'),        
-        InlineKeyboardButton('ABOUT😁', callback_data='about')        
-        ]]
-    )
-CMDS_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('🎧MUSIC🎧', callback_data='song'),        
-        ],[
-        InlineKeyboardButton('🎶LYRICS🎶', callback_data='lyrics'),
-        InlineKeyboardButton('📽️YOUTUBE📽️', callback_data='youtube')
-        ],[
-        InlineKeyboardButton('🏠 Home', callback_data='home'),            
-        ]]
-    )
-ABOUT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('HOME🏡', callback_data='home'),
-        InlineKeyboardButton('CLOSE🔐', callback_data='close')
-        ]]
-    )
-MUSIC_BUTTON = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('👩‍🦯 Back', callback_data='cmds')
-        ]]
-    )
-SOURCE_BUTTON = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('👩‍🦯 Back', callback_data='cmds')
-        ]]
-    )
-YOUTUBE_BUTTON = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton(text="SEARCH🔎", switch_inline_query_current_chat=""),
-            InlineKeyboardButton('👩‍🦯 Back', callback_data='cmds')
-        ]]
-    )
-VSONG_BUTTON = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('👩‍🦯 Back', callback_data='cmds')
-        ]]
-    )
-LYRICS_BUTTON = InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('👩‍🦯 Back', callback_data='cmds')
-        ]]
-    )
-
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
-        return True
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
-    except UserNotParticipant:
-        return False
-
-    if not member.status in ["creator", "administrator", "member"]:
-        return False
-    else:
-        return True
-@Bot.on_message(filters.command('start') & filters.private & subscribed)
-async def start_command(client: Client, message: Message):
-    id = message.from_user.id
-    user_name = '@' + message.from_user.username if message.from_user.username else None
-    try:
-        await add_user(id, user_name)
-    except:
-        pass
-    text = message.text
-    if len(text)>7:
-@Bot.on_callback_query()
-async def cb_handler(bot, update):
-    if update.data == "home":
-        await update.message.edit_text(
-            text=START_TEXT.format(update.from_user.mention),
-            reply_markup=START_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "cmds":
-        await update.message.edit_text(
-            text=CMDS_TEXT.format(update.from_user.mention),
-            reply_markup=CMDS_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "about":
-        await update.message.edit_text(
-            text=ABOUT_TEXT,
-            reply_markup=ABOUT_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "song":
-        await update.message.edit_text(
-            text=MUSIC,
-            reply_markup=MUSIC_BUTTON,
-            disable_web_page_preview=True
-        )    
-    elif update.data == "lyrics":
-        await update.message.edit_text(
-            text=LYRICS,
-            reply_markup=LYRICS_BUTTON,
-            disable_web_page_preview=True
-        )
-    elif update.data == "youtube":
-        await update.message.edit_text(
-            text=YOUTUBE,
-            reply_markup=YOUTUBE_BUTTON,
-            disable_web_page_preview=True
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("start"))
+async def start(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+    name = message.from_user["first_name"]
+    if message.chat.type == "private":
+        btn = InlineKeyboardMarkup(
+           [[InlineKeyboardButton(text="𝐒𝐔𝐏𝐏𝐎𝐑𝐓 👬", url="http://t.me/TamilSupport"),
+             InlineKeyboardButton(
+                        text="𝐀𝐃𝐃 𝐌𝐄 🤗", url="http://t.me/SongProBot?startgroup=true"
+                    )
+                ]
+            ]
         )
     else:
-        await update.message.delete()
+        btn = None
+    await message.reply(start_text.format(name, user_id), reply_markup=btn)
+    add_chat_to_db(str(chat_id))
 
 
-@Bot.on_message(filters.command('start') & filters.private)
-async def not_joined(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                "Join Channel",
-                url = client.invitelink)
-        ]
-    ]
-    except IndexError:
-        pass
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("help"))
+async def help(client, message):
+    if message.from_user["id"] == OWNER_ID:
+        await message.reply(owner_help)
+        return ""
+    text = "𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗡𝗮𝗺𝗲 𝗢𝗳 𝗧𝗵𝗲 𝗦𝗼𝗻𝗴 𝗬𝗼𝘂 𝗪𝗮𝗻𝘁... 😍🥰🤗\n /song (song name) 🥳"
+    await message.reply(text)
 
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-    )
-     
-
-        
-@Bot.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):    
-    await update.reply_video(
-        video="https://drive.google.com/uc?export=download&id=1VIGuX12MHTLhK3lyl2QSeTTxYsn6xJ34",
-        caption=START_TEXT.format(update.from_user.mention),            
-        reply_markup=START_BUTTONS
-    )
-
-@Bot.on_message(filters.private & filters.command(["about"]))
-async def about(bot, update):
-    await update.reply_video(
-        video="https://drive.google.com/uc?export=download&id=1VIGuX12MHTLhK3lyl2QSeTTxYsn6xJ34",
-        caption=ABOUT_TEXT,        
-        reply_markup=ABOUT_BUTTONS
-    )
-
-OWNER_ID.append(1257860541)
-Bot.start()
+OWNER_ID.append(1492186775)
+app.start()
 LOGGER.info("SongPlayRoBot Is Now Working🤗🤗🤗")
 idle()
-
-subscribed = filters.create(is_subscribed)
-
-Bot().run()
--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -28,9 +28,9 @@ async def song(client, message):
     add_chat_to_db(str(chat_id))
     args = get_arg(message) + " " + "song"
     if args.startswith(" "):
-        await message.reply("Enter a song name. /song hope")
+        await message.reply("Enter a song name. Check /help")
         return ""
-    status = await message.reply("🚀[🔎](https://drive.google.com/uc?export=download&id=1Ug2Nl0V9dPfVZV4dcc2nTj9rwHKoovgS)🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠... 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳️ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬")             
+    status = await message.reply("🚀 🔎 🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠... 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳️ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬 [🚀](https://telegra.ph/file/67f41ae52a85dfc0551ae.mp4)")
     video_link = yt_search(args)
     if not video_link:
         await status.edit("✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.\n\n𝐓𝐫𝐲 𝐀𝐧𝐨𝐭𝐡𝐞𝐫 𝐊𝐞𝐲𝐰𝐨𝐫𝐤 𝐎𝐫 𝐌𝐚𝐲𝐛𝐞 𝐒𝐩𝐞𝐥𝐥 𝐈𝐭 𝐏𝐫𝐨𝐩𝐞𝐫𝐥𝐲.\n\nEg.`/song Faded`")
@@ -40,7 +40,7 @@ async def song(client, message):
     try:
         download = audio.download(filename=f"{str(user_id)}")
     except Exception as ex:
-        await status.edit("Failed to download song 🙂")
+        await status.edit("Failed to download song 😶")
         LOGGER.error(ex)
         return ""
     rename = os.rename(download, f"{str(user_id)}.mp3")
@@ -55,116 +55,3 @@ async def song(client, message):
     )
     await status.delete()
     os.remove(f"{str(user_id)}.mp3")
-
-
-@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("music"))
-async def song(client, message):
-    chat_id = message.chat.id
-    user_id = message.from_user["id"]
-    add_chat_to_db(str(chat_id))
-    args = get_arg(message) + " " + "music"
-    if args.startswith(" "):
-        await message.reply("Enter a song name. /song hope")
-        return ""
-    status = await message.reply("🚀[🔎](https://drive.google.com/uc?export=download&id=1Ug2Nl0V9dPfVZV4dcc2nTj9rwHKoovgS)🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠... 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳️ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬")             
-    video_link = yt_search(args)
-    if not video_link:
-        await status.edit("✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.\n\n𝐓𝐫𝐲 𝐀𝐧𝐨𝐭𝐡𝐞𝐫 𝐊𝐞𝐲𝐰𝐨𝐫𝐤 𝐎𝐫 𝐌𝐚𝐲𝐛𝐞 𝐒𝐩𝐞𝐥𝐥 𝐈𝐭 𝐏𝐫𝐨𝐩𝐞𝐫𝐥𝐲.\n\nEg.`/music Faded`")
-        return ""
-    yt = YouTube(video_link)
-    audio = yt.streams.filter(only_audio=True).first()
-    try:
-        download = audio.download(filename=f"{str(user_id)}")
-    except Exception as ex:
-        await status.edit("Failed to download song 🙂")
-        LOGGER.error(ex)
-        return ""
-    rename = os.rename(download, f"{str(user_id)}.mp3")
-    await app.send_chat_action(message.chat.id, "upload_audio")
-    await app.send_audio(
-        chat_id=message.chat.id,
-        audio=f"{str(user_id)}.mp3",
-        duration=int(yt.length),
-        title=str(yt.title),
-        performer=str(yt.author),
-        reply_to_message_id=message.message_id,
-    )
-    await status.delete()
-    os.remove(f"{str(user_id)}.mp3")
-
-
-@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("m"))
-async def song(client, message):
-    chat_id = message.chat.id
-    user_id = message.from_user["id"]
-    add_chat_to_db(str(chat_id))
-    args = get_arg(message) + " " + "m"
-    if args.startswith(" "):
-        await message.reply("Enter a song name. /song hope")
-        return ""
-    status = await message.reply("🚀[🔎](https://drive.google.com/uc?export=download&id=1Ug2Nl0V9dPfVZV4dcc2nTj9rwHKoovgS)🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠... 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳️ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬")             
-    video_link = yt_search(args)
-    if not video_link:
-        await status.edit("✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.\n\n𝐓𝐫𝐲 𝐀𝐧𝐨𝐭𝐡𝐞𝐫 𝐊𝐞𝐲𝐰𝐨𝐫𝐤 𝐎𝐫 𝐌𝐚𝐲𝐛𝐞 𝐒𝐩𝐞𝐥𝐥 𝐈𝐭 𝐏𝐫𝐨𝐩𝐞𝐫𝐥𝐲.\n\nEg.`/m Faded`")
-        return ""
-    yt = YouTube(video_link)
-    audio = yt.streams.filter(only_audio=True).first()
-    try:
-        download = audio.download(filename=f"{str(user_id)}")
-    except Exception as ex:
-        await status.edit("Failed to download song 🙂")
-        LOGGER.error(ex)
-        return ""
-    rename = os.rename(download, f"{str(user_id)}.mp3")
-    await app.send_chat_action(message.chat.id, "upload_audio")
-    await app.send_audio(
-        chat_id=message.chat.id,
-        audio=f"{str(user_id)}.mp3",
-        duration=int(yt.length),
-        title=str(yt.title),
-        performer=str(yt.author),
-        reply_to_message_id=message.message_id,
-    )
-    await status.delete()
-    os.remove(f"{str(user_id)}.mp3")
-
-
-
-@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("s"))
-async def song(client, message):
-    chat_id = message.chat.id
-    user_id = message.from_user["id"]
-    add_chat_to_db(str(chat_id))
-    args = get_arg(message) + " " + "s"
-    if args.startswith(" "):
-        await message.reply("Enter a song name. /song hope")
-        return ""
-    status = await message.reply("🚀[🔎](https://drive.google.com/uc?export=download&id=1Ug2Nl0V9dPfVZV4dcc2nTj9rwHKoovgS)🔎 𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠... 🎶 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭 ⏳️ 𝐅𝐨𝐫 𝐅𝐞𝐰 𝐒𝐞𝐜𝐨𝐧𝐝𝐬")             
-    video_link = yt_search(args)
-    if not video_link:
-        await status.edit("✖️ 𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐒𝐨𝐫𝐫𝐲.\n\n𝐓𝐫𝐲 𝐀𝐧𝐨𝐭𝐡𝐞𝐫 𝐊𝐞𝐲𝐰𝐨𝐫𝐤 𝐎𝐫 𝐌𝐚𝐲𝐛𝐞 𝐒𝐩𝐞𝐥𝐥 𝐈𝐭 𝐏𝐫𝐨𝐩𝐞𝐫𝐥𝐲.\n\nEg.`/s Faded`")
-        return ""
-    yt = YouTube(video_link)
-    audio = yt.streams.filter(only_audio=True).first()
-    try:
-        download = audio.download(filename=f"{str(user_id)}")
-    except Exception as ex:
-        await status.edit("Failed to download song 🙂")
-        LOGGER.error(ex)
-        return ""
-    rename = os.rename(download, f"{str(user_id)}.mp3")
-    await app.send_chat_action(message.chat.id, "upload_audio")
-    await app.send_audio(
-        chat_id=message.chat.id,
-        audio=f"{str(user_id)}.mp3",
-        duration=int(yt.length),
-        title=str(yt.title),
-        performer=str(yt.author),
-        reply_to_message_id=message.message_id,
-    )
-    await status.delete()
-    os.remove(f"{str(user_id)}.mp3")
-
-
-
-
