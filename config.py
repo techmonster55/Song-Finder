25c880496e465b69bc30d6bd0ab84f20cbff8c20
sarangsahajan <88932889+sarangsahajan@users.noreply.github.com>
2021-12-30 03:16:29 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -8,3 +10,7 @@ OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 #Force sub message 
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
